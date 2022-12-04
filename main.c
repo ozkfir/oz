1eaa6706888ca78e04498ab0ad2f9e2427f6725b
ozkfir <rccvvccr@gmail.com>
2022-12-04 12:30:48 +0200

    -a

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "my_mat.h"
+#define Len 10
+
+
+int main()
+{
+    int mat[Len][Len];
+    char command;
+    scanf("%c",&command);
+    while(command!='D')
+    {
+        if(command=='A')
+        {
+           build_mat(mat);
+
+        }
+        else if(command=='B')
+        {
+
+            int i, j;
+            scanf("%d%d", &i, &j);
+            if(way(mat,i,j)!=0)
+                printf("True\n");
+                else
+                printf("False\n");
+
+        }
+             else
+             {
+                int i, j;
+                scanf("%d%d", &i, &j);
+                 printf("%d\n",length_way(mat,i,j));
+             }
+    }
+    return 0;
+}
+
